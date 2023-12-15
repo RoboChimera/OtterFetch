8266236dce2ca69b5ab0a70a758c2e97488f1e61
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2023-12-15 20:19:07 +0100

    config file

--- config.h
+++ config.h
@@ -1,27 +1,27 @@
 /*
- * function		description		function name
+ * function		description		function name		library directory/file
  *
- * OS + Version		uname -sr		fetchUname()
+ * OS + Version		uname -sr		fetchUname()		components/fetchUname.h
  *
- * totalram		total memory in GB	fetchTotalram()
- * freeram		free memory in GB	fetchFreeram()
+ * totalram		total memory in GB	fetchTotalram()		components/fetchTotalRam.h
+ * freeram		free memory in GB	fetchFreeram()		components/fetchUname.h
 */
 
+// Include only variables we need
 #include "components/fetchTotalRam.h"
 #include "components/fetchFreeRam.h"
 #include "components/fetchUname.h"
 
 void fetch(GtkWidget *label) {
-	// Functions we want to call
 	char *totalram = fetchTotalram();
 	char *freeram = fetchFreeram();
 	char *version = fetchUname();
 
-	// Configurate the GTK label
+	// Configurates the GTK label
 	char *displayText = g_strdup_printf("%s%s%s", version, totalram, freeram);
 	gtk_label_set_text(GTK_LABEL(label), displayText);
 
-	// Free variables when no longer needed
+	// Frees variables when no longer needed
 	free(version);
 	free(totalram);
 	free(freeram);
