f26f050c9ca531b2f72d74cd49907cc37fb7fe41
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2023-12-26 16:04:27 +0100

    should work on openBSD now

--- components/fetchCpuThreads.h
+++ components/fetchCpuThreads.h
@@ -7,10 +7,25 @@
 
 char* fetchCpuThreads(void) {
 #if defined(__OpenBSD__) || defined(__NetBSD__) || defined(__FreeBSD__)
+	int cpuThreads_mib[2];
+	size_t cpuThreads_len;
+	int ncpu;
+
+	cpuThreads_mib[0] = CTL_HW;
+	cpuThreads_mib[1] = HW_NCPUFOUND;
+
+	cpuThreads_len = sizeof(ncpu);
+
+	if (sysctl(cpuThreads_mib, 2, &ncpu, &cpuThreads_len, NULL, 0) == -1) {
+		perror("sysctl");
+		exit(EXIT_FAILURE);
+	}
 #else
-	int cpuThreadsCSize = snprintf(NULL, 0, "CPU Cores: %d\n", get_nprocs()) + 1;
-	char *cpuThreads = (char *)malloc(cpuThreadsCSize);
-	sprintf(cpuThreads, "CPU Cores: %d\n", get_nprocs());
+	int ncpu = get_nprocs();
 #endif
+	int cpuThreadsCSize = snprintf(NULL, 0, "CPU Cores: %d\n", ncpu + 1);
+	char *cpuThreads = (char *)malloc(cpuThreadsCSize);
+	sprintf(cpuThreads, "CPU Cores: %d\n", ncpu);
+
 	return cpuThreads;
 }
