0cf2e7b9c8b80af69ba83d1ff0a810785d404581
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2023-12-27 13:36:32 +0100

    back to default config

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
  * function description		function name		library directory/file		notes
  *
  * OS + Version			fetchUname()		components/fetchUname.h
- * Uptime in Hours,		fetchUptime()		components/fetchUptime.h	Currently Only on Linux
+ * Uptime in Hours,		fetchUptime()		components/fetchUptime.h
  * Minutes and Seconds
  *
  * Total memory in GB		fetchTotalram()		components/fetchTotalRam.h
@@ -15,22 +15,19 @@
 #include "components/fetchTotalRam.h"
 #include "components/fetchFreeRam.h"
 #include "components/fetchUname.h"
-#include "components/fetchUptime.h"
 
 void fetch(GtkWidget *label) {
 	char *totalram = fetchTotalram();
 	char *freeram = fetchFreeram();
 	char *version = fetchUname();
-	char *uptime = fetchUptime();
 
 	// Configurates the GTK label
-	char *displayText = g_strdup_printf("%s%s%s%s", version, totalram, freeram, uptime);
+	char *displayText = g_strdup_printf("%s%s%s", version, totalram, freeram);
 	gtk_label_set_text(GTK_LABEL(label), displayText);
 
 	// Frees variables when no longer needed
 	free(version);
 	free(totalram);
 	free(freeram);
-	free(uptime);
 	g_free(displayText);
 }
