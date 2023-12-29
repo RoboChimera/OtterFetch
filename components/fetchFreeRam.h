0ffbffff4e8f53473f34c037aef56a6391cbf67f
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2023-12-29 20:30:18 +0100

    maybe don't use vim to change everything

--- components/fetchFreeRam.h
+++ components/fetchFreeRam.h
@@ -14,28 +14,28 @@ char* fetchFreeram(void) {
 
 	float freeramValue = sInfo.freeram / 1000 / 1000;
 #elif __sun
-	freeramKstat_ctl_t *freeramKctl;
-	freeramKstat_t *freeramKstat;
+	kstat_ctl_t *freeramKctl;
+	kstat_t *freeramKstat;
 
-	if ((freeramKctl = freeramKstat_open()) == NULL) {
+	if ((freeramKctl = kstat_open()) == NULL) {
 		perror("freeramKstat_open");
 	}
 
-	freeramKstat = freeramKstat_lookup(freeramKctl, "unix", 0, "system_pages");
+	freeramKstat = kstat_lookup(freeramKctl, "unix", 0, "system_pages");
 	if (freeramKstat == NULL) {
 		perror("freeramKstat_lookup");
-		freeramKstat_close(freeramKctl);
+		kstat_close(freeramKctl);
 	}
 
-	if (freeramKstat_read(freeramKctl, freeramKstat, NULL) == -1) {
-		perror("freeramKstat_read");
-		freeramKstat_close(freeramKctl);
+	if (kstat_read(freeramKctl, freeramKstat, NULL) == -1) {
+		perror("kstat_read");
+		kstat_close(freeramKctl);
 	}
 
-	freeramKstat_named_t *freeramKname = freeramKstat_data_lookup(freeramKstat, "availrmem");
+	freeramKstat_named_t *freeramKname = kstat_data_lookup(freeramKstat, "availrmem");
 	if (freeramKname == NULL) {
 		perror("freeramKstat_data_lookup");
-		freeramKstat_close(freeramKctl);
+		kstat_close(freeramKctl);
 	}
 
 	uint64_t freeramUint = freeramKname->value.i64;
