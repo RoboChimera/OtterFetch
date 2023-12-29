38da8d13b277f971e480a74bfa2395423beeb27c
HelloRoboRobo <gitlab@HelloRoboRobo.com>
2023-12-29 14:51:45 +0100

    I heard from someone that this would be a better implementation

--- components/fetchTotalRam.h
+++ components/fetchTotalRam.h
@@ -1,37 +1,11 @@
-#ifdef __linux__
-	#include <sys/sysinfo.h>
-#else
-	#include <sys/types.h>
-	#include <sys/sysctl.h>
-#endif
+#include <unistd.h>
 
 char* fetchTotalram(void) {
-#ifdef __linux__
-	struct sysinfo sInfo;
-	sysinfo(&sInfo);
+	long pages = sysconf(_SC_PHYS_PAGES);
+	long pageSize = sysconf(_SC_PAGE_SIZE);
+	long long physRam = pages * pageSize;
 
-	float totalramValue = sInfo.totalram / 1000 / 1000;
-#else
-	int totalram_mib[2];
-	size_t totalramLen;
-	uint64_t totalramUint;
-
-	// Get the physical RAM size
-	totalram_mib[0] = CTL_HW;
-#ifdef __OpenBSD__
-	totalram_mib[1] = HW_PHYSMEM64;
-#else
-	totalram_mib[1] = HW_PHYSMEM;
-#endif
-
-	totalramLen = sizeof(totalramUint);
-	if (sysctl(totalram_mib, 2, &totalramUint, &totalramLen, NULL, 0) == -1) {
-		perror("sysctl");
-		exit(EXIT_FAILURE);
-	}
-
-	float totalramValue = totalramUint / 1000 / 1000;
-#endif
+	float totalramValue = physRam / 1024 / 1024;
 
 	int totalramCSize = snprintf(NULL, 0, "Built-in memory: %.1f MB\n", totalramValue) + 1;
 	char *totalram = (char *)malloc(totalramCSize);
